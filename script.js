a3d25e77a1f9bb9c6f9f853ae10a9cdd596d03f9
Sajirupan <balabavansajirupan@gmail.com>
2025-11-10 11:37:14 +0530

    change

--- script.js
+++ script.js
@@ -1,49 +1,3 @@
-// console.log("Sajirupan");
-// console.log("Puttur");
-// console.log("Age-21");
-// console.log("Trip");
-
-
-// console.log(Nuw);
-// var Nuw = 24;
-
-
-
-// console.log("5+7");
-// document.getElementById("text").innerHTML="Hello World";
-// document.write("This is a popular Web page");
-// window.alert("You are a wrong member");
-// console.log("Java Script is fun");
-// const name="Saji";
-// var age="21";
-// let subject="Maths";
-// console.log(name);
-// console.log(age);
-// console.log(subject);
-// var favcolor =prompt("inout your favorite color");
-// console.log(favcolor);
-// document.body.style.backgroundColor = favcolor;
-// let price = 500;
-// let quanity =150;
-// let display = price * quanity;
-// console.log(display);
-// let color;
-// console.log(color);
-// let firstname = "Saji";
-// let lastname = "Rupan";
-// console.log("My name is"+" "+firstname +" "+ lastname);
-// let age = "21" ;
-// console.log(typeof age);
-
-
-// Question Number 3
-// let score=10;
-// score=15;
-// document.getElementById("text").innerHTML=score;
-
-
-
-
 // JavaScript Classwork 8.1
 
 // Question Number 1
@@ -71,7 +25,7 @@ console.log(fav_fruit);
 
 // Question Number 6
 let message="Welcome to the JavaScript class!";
-document.write("<br> Message");
+document.write("<br><br> Message");
 
 // Question Number 7
 let name=prompt("Enter user name");
@@ -84,36 +38,36 @@ console.log(fav_color);
 // Question Number 8
 let first_name=prompt("Enter your first name");
 let last_name=prompt("Enter your last name");
-document.write("<br> Hello,"+" "+ first_name + " " + last_name + "! Welcome to JavaScript class.");
+document.write("<br><br> Hello,"+" "+ first_name + " " + last_name + "! Welcome to JavaScript class.");
 
 
 // Question Number 9
 let birth_year=prompt("Enter Birth Year");
 let current_year=2025;
 let age=current_year-birth_year;
-document.write("<br> You are"+" "+age+" "+ "years old!");
+document.write("<br><br> You are"+" "+age+" "+ "years old!");
 
 
 // Question Number 10
 let length=prompt("Enter length");
 let width=prompt("Enter width");
 let area=length*width;
-document.write("<br> Ravi's land area is"+" "+ area +" "+ "square units.");
+document.write("<br><br> Ravi's land area is"+" "+ area +" "+ "square units.");
 
 // Question Number 11
 let price=prompt("Enter item price");
 let quantity=prompt("Enter quantity");
 let total=price*quantity;
-document.write("<br> Total price is"+" "+ total +" "+ "rupees.");
+document.write("<br><br> Total price is"+" "+ total +" "+ "rupees.");
 
 // Question Number 12
 let ticket_price=prompt("Enter ticket price");
 let sold_tickets=prompt("Enter number of tickets sold");
 let amount=ticket_price*sold_tickets;
-document.write("<br> Total revenue is"+" "+ amount +" "+ "rupees.");
+document.write("<br><br> Total revenue is"+" "+ amount +" "+ "rupees.");
 
 // Question Number 13
 let total_apple=prompt("Enter Total Apples");
 let per_basket=prompt("Enter number of apples per basket");
 let number=total_apple/per_basket;
-document.write("<br> Total baskets needed are" +" "+ number+".");
+document.write("<br><br> Total baskets needed are" +" "+ number+".");
